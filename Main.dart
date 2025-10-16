a368d2b857d8f54e24488138777f467c5a74b6cb
BALIJI RAMSAI KUMAR <110591533+rahul0rsk@users.noreply.github.com>
2025-10-16 13:20:56 +0530

    Initialize Flutter app with CoinFlip widget

---
+++ Main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'coinflip.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CoinFlip()
+  ));
+}
